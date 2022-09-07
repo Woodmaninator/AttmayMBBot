655bdc41cc7ae190b8eb15f11a4a16aa58886d02
Woodmaninator <woodmaninator@gmail.com>
2022-09-08 00:11:07 +0200

    Re-enabled the NotFoundCommand

--- src/main/java/attmayMBBot/commands/NotFoundCommand.java
+++ src/main/java/attmayMBBot/commands/NotFoundCommand.java
@@ -5,6 +5,6 @@ import discord4j.core.object.entity.Message;
 public class NotFoundCommand implements ICommand{
     @Override
     public void execute(Message message, String[] args) {
-        //message.getChannel().block().createMessage("I have no idea what you want me to do...").block();
+        message.getChannel().block().createMessage("I have no idea what you want me to do...").block();
     }
 }
