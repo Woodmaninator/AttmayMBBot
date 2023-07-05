e585b586ddbd06a5a786b7c707108eaaf2f93a21
mantelis3000 <86557631+mantelis3000@users.noreply.github.com>
2023-07-05 19:33:55 +0300

    Changed !commands link to the list in README file

--- src/main/java/attmayMBBot/commands/CommandsCommand.java
+++ src/main/java/attmayMBBot/commands/CommandsCommand.java
@@ -5,6 +5,6 @@ import discord4j.core.object.entity.Message;
 public class CommandsCommand implements ICommand{
     @Override
     public void execute(Message message, String[] args) {
-        message.getChannel().block().createMessage("You can find a list of all the commands on the github page for this project:\nhttps://github.com/Woodmaninator/AttmayMBBot").block();
+        message.getChannel().block().createMessage("You can find a list of all the commands on the github page for this project:\nhttps://github.com/Woodmaninator/AttmayMBBot#commands").block();
     }
 }
